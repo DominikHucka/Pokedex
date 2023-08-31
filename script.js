c8b13bf97933c16d9cad09167de6493569229aa0
Dominik Hucka <dominikhucka@googlemail.com>
2023-08-31 21:56:26 +0200

    update header-added hover function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentPokemon;
 let allPokemons;
 let allPokemonUrl = []; //sind alle URL von der Pokemon API 
+let limitPokemon = 99;
 
 
 async function init() {
@@ -27,9 +28,10 @@ async function includeHTML() {
 
 
 async function loadAllPokemons() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=31&offset=0'; // API limit auf 30 gesetzt 
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0'; // API limit auf 30 gesetzt 
     let response = await fetch(url); //ladet die API runter (aller Pokemons)
     allPokemons = await response.json(); //wandelt das in eine JSON um
+    console.log('show all Pokemons', allPokemons);
     let result = allPokemons['results']; //results ist ein Array in der API 
     loadUrl(result);
     loadPokemon();
@@ -60,28 +62,30 @@ async function loadPokemon() {
 function renderPokemonInformation() {
     let pokemonImage = currentPokemon['sprites']['other']['official-artwork']['front_default'];
     let pokemonName = currentPokemon['forms']['0']['name'];
-    let modifiedName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+    let modifiedName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1); //anfangsbuchstaben wird in einen Große umgewandelt
     let pokemonId = currentPokemon['id'];
     let pokemonType = currentPokemon['types']['0']['type']['name'];
-    document.getElementById('pokedex').innerHTML += generateHTMLPokedex(pokemonImage, modifiedName, pokemonId, pokemonType);
+    let modifiedType = pokemonType.charAt(0).toUpperCase() + pokemonType.slice(1);
+    let backgroundColorClass = `bg-${pokemonType.toLowerCase()}`; // platzhalter für die CSS klasse bg-.....CSS ab Zeile 135
+    let shadowClass = `shadow-${pokemonType.toLowerCase()}`;
+    document.getElementById('pokedex').innerHTML += generateHTMLPokedex(pokemonImage, modifiedName, pokemonId, modifiedType, backgroundColorClass, shadowClass);
 }
 
 
-function generateHTMLPokedex(img, name, id, type) {
+function generateHTMLPokedex(img, name, id, type, color, shadow) {
     return /*html*/`
-        <div  class="pokemon-card">
+        <div  class="pokemon-card ${shadow}" >
             <div class="pokemon-card-img">
                 <img src="${img}" alt="Pokemon Image">
             </div>
             <div class="pokemon-card-information">
-                <p class="pokemon-id">Nr. ${id}</p>
+                <p class="pokemon-id"># ${id}</p>
                 <h3>${name}</h3>
-                <div class="pokemon-card-type normal">
+                <div id="typeColor" class="pokemon-card-type ${color}">
                     <p>${type}</p>
                 </div>
             </div>
         </div>
     `
 }
-
 
